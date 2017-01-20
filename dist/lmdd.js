40249de5b378bfb72e6b3e75669d16b9555d5f70
Yair Levy <supraniti.inbox@gmail.com>
2017-01-20 15:02:03 +0200

    wip

--- dist/lmdd.js
+++ dist/lmdd.js
@@ -1,36 +1,6 @@
 
 
 ////todos:
-function touchHandler(event)
-{
-    var touches = event.changedTouches,
-        first = touches[0],
-        type = "";
-    switch(event.type)
-    {
-        case "touchstart": type = "mousedown"; break;
-        case "touchmove":  type = "mousemove"; break;
-        case "touchend":   type = "mouseup";   break;
-        default:           return;
-    }
-
-    // initMouseEvent(type, canBubble, cancelable, view, clickCount,
-    //                screenX, screenY, clientX, clientY, ctrlKey,
-    //                altKey, shiftKey, metaKey, button, relatedTarget);
-
-    var simulatedEvent = document.createEvent("MouseEvent");
-    simulatedEvent.initMouseEvent(type, true, true, window, 1,
-        first.screenX, first.screenY,
-        first.clientX, first.clientY, false,
-        false, false, false, 0/*left*/, null);
-
-    first.target.dispatchEvent(simulatedEvent);
-    event.preventDefault();
-}
-    document.addEventListener("touchstart", touchHandler, true);
-    document.addEventListener("touchmove", touchHandler, true);
-    document.addEventListener("touchend", touchHandler, true);
-    document.addEventListener("touchcancel", touchHandler, true);
 
 var lmdd = (function() {
     var scope = {};
@@ -346,11 +316,16 @@ var lmdd = (function() {
                 draggables[i].addEventListener("mousedown", function(event) {
                     dragStarted(event, el);
                 }, false); //reverse
+                draggables[i].addEventListener("touchstart", function(event) {
+                    dragStarted(event, el);
+                }, false); //reverse
             }
             //record mouse movements
             document.addEventListener("mousemove", mouseLocationUpdated); //reverse
+            document.addEventListener("touchmove", mouseLocationUpdated);//reverse
             document.addEventListener("scroll", mouseLocationUpdated);
             window.addEventListener("mouseup", dragEnded); //reverse
+            document.addEventListener("touchend", dragEnded);
             //create animation object
             el.animation = new animation(el);
             //el.range = document.createRange(); // for later performance enhancement
