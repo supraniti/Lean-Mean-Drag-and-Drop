abb3fa90976d7bf8dde8977b858fc305ede91607
Yair Levy <supraniti.inbox@gmail.com>
2017-03-29 22:49:49 +0300

    Version 1.0

--- gh-pages/script.js
+++ gh-pages/script.js
@@ -1,10 +1,6 @@
 /**
  * Created by יאיר on 18/01/2017.
  */
-lmdd.set(document.getElementById('clonner-example'), {
-    containerClass: 'example-container',
-    draggableItemClass: 'example-item'
-});
 lmdd.set(document.getElementById('simple-grid-example'), {
     containerClass: 'simple-grid',
     draggableItemClass: 'grid-item'
@@ -13,10 +9,6 @@ lmdd.set(document.getElementById('nested-example'), {
     containerClass: 'nestable',
     draggableItemClass: 'nested-item',
     positionDelay:true
-});
-lmdd.set(document.getElementById('nested-tree-example'), {
-    containerClass: 'nestable-ul',
-    draggableItemClass: 'nested-li'
 });
 lmdd.set(document.getElementById('handle-example'), {
     containerClass: 'handle-grid',
